9e8a42d303d51436690071a9cd2f9c2e7d729874
hongyuezhilian <7924503+hongyuezhilian@user.noreply.gitee.com>
2023-06-01 16:47:32 +0800

    结构大改

---
+++ js/login.js
@@ -0,0 +1,34 @@
+// JavaScript Document	
+ 		function getValue() {
+			var a = jQuery("#account").val();
+			var p = jQuery("#pwd").val();
+			// 发送AJAX POST请求
+            // 1. 创建AJAX核心对象
+            var xhr = new XMLHttpRequest();
+            // 2. 注册回调函数
+            xhr.onreadystatechange = function (){
+                if (this.readyState == 4) {
+                    if (this.status == 200) {
+						var res = xhr.responseText;
+						var obj = JSON.parse(res);
+						if(obj.code==1){
+							window.sessionStorage.setItem("CCNtoken",obj.data);
+							alert(window.sessionStorage.getItem("CCNtoken"));
+							window.location.href="../index?token="+window.sessionStorage.getItem("CCNtoken");
+						}
+						else{
+							alert("false");
+						}
+                    }else{
+                        alert(this.status);
+                    }
+                }
+            }
+            // 3. 开启通道
+            xhr.open("POST", "/login", true)
+ 			xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded")
+			xhr.send("mail="+a+"&password="+p)
+            // 4. 发送请求
+            xhr.send()
+		}
+            
