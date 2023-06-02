d966f7a91b6e77ad178824cf6c20b17517ecd7b7
hongyuezhilian <7924503+hongyuezhilian@user.noreply.gitee.com>
2023-06-02 09:24:35 +0800

    111

--- js/login.js
+++ js/login.js
@@ -22,7 +22,7 @@ function regist() {
 							var result="";
 							jQuery.ajax({
 								"type":"get",
-								"url":"../index",
+								"url":"../",
 								headers: {
         							token:  window.sessionStorage.getItem("CCNtoken")
     							},
